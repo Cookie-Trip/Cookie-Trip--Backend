5f98bc0028e7f230744fc4495dc3d9ffd2acb01d
pepe6bf <pepe6bf@gmail.com>
2023-05-09 12:30:08 +0900

    chore: 프로젝트 생성
    Language & Framework
    - Java 17
    - Spring Boot 2.7.11
    
    Dependency
    - starter-web
    - data-jpa
    - security
    - oauth2-client
    - MySQL
    - H2
    - Lombok

---
+++ cookie-Trip/src/main/java/com/cookietrip/CookieTripApplication.java
@@ -0,0 +1,13 @@
+package com.cookietrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookieTripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookieTripApplication.class, args);
+	}
+
+}
---
+++ cookie-Trip/src/test/java/com/cookietrip/CookieTripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cookietrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookieTripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
