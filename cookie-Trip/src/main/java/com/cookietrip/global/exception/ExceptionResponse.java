0905b5a0afe4182e2ad242322dc2d715de16189c
pepe6bf <pepe6bf@gmail.com>
2023-05-10 13:31:19 +0900

    feat: 예외 처리를 위한 Exception 관련 Class 생성
    예외 처리에 활용할 Exception 관련 Class들을 생성해주었다.
    
    - ExceptionCode : 예외 정보를 Enum으로 관리하기 위한 최상위 Interface
    - GlobalException : 공통 예외 정보를 기술한 Enum
    - ExceptionResposne : API 예외 응답을 담을 DTO

---
+++ cookie-Trip/src/main/java/com/cookietrip/global/exception/ExceptionResponse.java
@@ -0,0 +1,134 @@
+package com.cookietrip.global.exception;
+
+import com.cookietrip.global.util.GsonUtil;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.validation.BindingResult;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ExceptionResponse {
+
+    private Integer status;
+    private String code;
+    private String message;
+    private List<FieldError> errors;
+    private LocalDateTime timestamp;
+
+    private ExceptionResponse(
+            final ExceptionCode exceptionCode
+    ) {
+        this.message = exceptionCode.getMessage();
+        this.status = exceptionCode.getHttpStatus().value();
+        this.code = exceptionCode.getCode();
+        this.timestamp = LocalDateTime.now();
+        this.errors = new ArrayList<>();
+    }
+
+    private ExceptionResponse(
+            final ExceptionCode exceptionCode,
+            final String message
+    ) {
+        this.message = message;
+        this.status = exceptionCode.getHttpStatus().value();
+        this.code = exceptionCode.getCode();
+        this.timestamp = LocalDateTime.now();
+        this.errors = new ArrayList<>();
+    }
+
+    private ExceptionResponse(
+            final ExceptionCode exceptionCode,
+            final List<FieldError> errors
+    ) {
+        this.message = exceptionCode.getMessage();
+        this.status = exceptionCode.getHttpStatus().value();
+        this.code = exceptionCode.getCode();
+        this.timestamp = LocalDateTime.now();
+        this.errors = errors;
+    }
+
+    public static ExceptionResponse of(
+            final ExceptionCode exceptionCode
+    ) {
+        return new ExceptionResponse(exceptionCode);
+    }
+
+    public static ExceptionResponse of(
+            final ExceptionCode exceptionCode,
+            final String message
+    ) {
+        return new ExceptionResponse(exceptionCode, message);
+    }
+
+    public static ExceptionResponse of(
+            final ExceptionCode code,
+            final BindingResult bindingResult
+    ) {
+        return new ExceptionResponse(code, FieldError.of(bindingResult));
+    }
+
+    public static ExceptionResponse of(
+            final ExceptionCode exceptionCode,
+            final List<FieldError> errors
+    ) {
+        return new ExceptionResponse(exceptionCode, errors);
+    }
+
+    /**
+     * Object -> Json (필터에서 사용)
+     */
+    public String convertJson() {
+        return new GsonUtil().toJson(this);
+    }
+
+    /**
+     * 입력 값 검증 에러를 상세하게 표현하는 Inner Class
+     */
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class FieldError {
+        private String field;
+        private String value;
+        private String reason;
+
+        private FieldError(
+                String field,
+                String value,
+                String reason
+        ) {
+            this.field = field;
+            this.value = value;
+            this.reason = reason;
+        }
+
+        public static List<FieldError> of(
+                final String field,
+                final String value,
+                final String reason
+        ) {
+            List<FieldError> fieldErrors = new ArrayList<>();
+            fieldErrors.add(new FieldError(field, value, reason));
+
+            return fieldErrors;
+        }
+
+        private static List<FieldError> of(
+                final BindingResult bindingResult
+        ) {
+            final List<org.springframework.validation.FieldError> fieldErrors = bindingResult.getFieldErrors();
+
+            return fieldErrors.stream()
+                    .map(error -> new FieldError(
+                            error.getField(),
+                            error.getRejectedValue() == null ? "" : error.getRejectedValue().toString(),
+                            error.getDefaultMessage()))
+                    .collect(Collectors.toList());
+        }
+    }
+}
