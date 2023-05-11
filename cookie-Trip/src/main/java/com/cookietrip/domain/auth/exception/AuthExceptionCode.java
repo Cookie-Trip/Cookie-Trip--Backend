0b854dab0cddcdb97e11861473bf89c34cef25e9
pepe6bf <pepe6bf@gmail.com>
2023-05-12 02:09:16 +0900

    exception: #10 인증 예외 code & handler 생성
    - 인증 예외를 표현할 AuthExceptionCode Enum Class를 개발하고 발생가능할 수 있다고 예상되는 예외들을 기술해주었다.
    - 인증 예외가 발생했을때 예외 처리를 진행할 Handler Class를 생성하였다.

---
+++ cookie-Trip/src/main/java/com/cookietrip/domain/auth/exception/AuthExceptionCode.java
@@ -0,0 +1,52 @@
+package com.cookietrip.domain.auth.exception;
+
+import com.cookietrip.global.exception.ExceptionCode;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+
+@Getter
+public enum AuthExceptionCode
+        implements ExceptionCode {
+
+    /**
+     * JWT
+     * 1 ~ 99
+     */
+    INVALID_TOKEN(UNAUTHORIZED, "AT-C-001", "유효하지 않은 토큰입니다."),
+    EXPIRED_TOKEN(UNAUTHORIZED, "AT-C-002", "만료된 토큰입니다."),
+    NOT_EXPIRED_TOKEN(BAD_REQUEST, "AT-C-003", "만료되지 않은 토큰입니다."),
+    REQUEST_TOKEN_NOT_FOUND(BAD_REQUEST, "AT-C-004", "요청에 토큰이 존재하지 않습니다."),
+    INVALID_REFRESH_TOKEN(BAD_REQUEST, "AT-C-005", "유효하지 않은 리프레쉬 토큰입니다."),
+
+    /**
+     * MEMBER
+     * 100 ~ 199
+     */
+    OAUTH_PROVIDER_MISMATCH(BAD_REQUEST, "AT-C-100", "일치하지 않는 로그인 제공자입니다."),
+    INVALID_MEMBER_ROLE(FORBIDDEN, "AT-C-101", "유효하지 않은 사용자 권한입니다."),
+    NOT_AUTHORIZATION_USER(NOT_FOUND, "AT-C-102", "인가된 사용자가 아닙니다."),
+
+    /**
+     * Common Exception
+     * 200 ~
+     */
+    AUTHENTICATION_ERROR(UNAUTHORIZED, "AT-C-200", "Authentication exception."),
+    INTERNAL_AUTHENTICATION_SERVICE_EXCEPTION(INTERNAL_SERVER_ERROR, "AT-S-200", "Internal authentication service exception.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
+    AuthExceptionCode(
+            HttpStatus httpStatus,
+            String code,
+            String message
+    ) {
+        this.httpStatus = httpStatus;
+        this.code = code;
+        this.message = message;
+    }
+}
---
+++ cookie-Trip/src/main/java/com/cookietrip/domain/auth/exception/AuthExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.cookietrip.domain.auth.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class AuthExceptionHandler {
+}
