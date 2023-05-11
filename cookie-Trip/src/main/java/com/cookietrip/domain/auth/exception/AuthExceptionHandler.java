f6a7420725776977547e29d325c2dcb6133455d1
pepe6bf <pepe6bf@gmail.com>
2023-05-12 02:21:39 +0900

    exception: #10 TokenException handler 등록
    TokenException을 AuthExceptionHandler에 등록해주었다.

--- cookie-Trip/src/main/java/com/cookietrip/domain/auth/exception/AuthExceptionHandler.java
+++ cookie-Trip/src/main/java/com/cookietrip/domain/auth/exception/AuthExceptionHandler.java
@@ -1,9 +1,28 @@
 package com.cookietrip.domain.auth.exception;
 
+import com.cookietrip.global.exception.ExceptionResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
 public class AuthExceptionHandler {
+
+    /**
+     * TokenException handling (Custom Exception)
+     */
+    @ExceptionHandler(TokenException.class)
+    protected ResponseEntity<ExceptionResponse> handleTokenException(
+            TokenException e
+    ) {
+        AuthExceptionCode exceptionCode = e.getAuthExceptionCode();
+        log.error("{}", e.getMessage());
+        return new ResponseEntity<>(
+                ExceptionResponse.of(exceptionCode, exceptionCode.getMessage()),
+                HttpStatus.valueOf(exceptionCode.getHttpStatus().value())
+        );
+    }
 }
