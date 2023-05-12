7eecc7cb442cb7c4b060b0dd15cebbfa6d208407
pepe6bf <pepe6bf@gmail.com>
2023-05-12 13:31:31 +0900

    feat: #10 findByPersonalId Query Method 개발
    MemberRepository Interface에 사용자의 개인 id를 기반으로 Member Entity를 조회하는 Query Method를 추가하였다.

--- cookie-Trip/src/main/java/com/cookietrip/domain/member/repository/MemberRepository.java
+++ cookie-Trip/src/main/java/com/cookietrip/domain/member/repository/MemberRepository.java
@@ -3,6 +3,8 @@ package com.cookietrip.domain.member.repository;
 import com.cookietrip.domain.member.model.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+import java.util.Optional;
 
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByPersonalId(String personalId);
 }
