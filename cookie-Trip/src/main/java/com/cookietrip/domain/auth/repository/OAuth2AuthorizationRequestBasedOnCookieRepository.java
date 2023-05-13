ad40bdeacd325c5fe640f6988bdf6833db2e6bfb
pepe6bf <pepe6bf@gmail.com>
2023-05-14 01:15:03 +0900

    refactor: #10 OAuth2AuthorizationRequestBasedOnCookieRepository 필드 값 설정 수정
    OAuth2AuthorizationRequestBasedOnCookieRepository의 필드값을 코드에 설정하는게 아닌 application.yml에 기술하고 해당 값을 불러와서 설정하는 방식으로 변경하였다.

--- cookie-Trip/src/main/java/com/cookietrip/domain/auth/repository/OAuth2AuthorizationRequestBasedOnCookieRepository.java
+++ cookie-Trip/src/main/java/com/cookietrip/domain/auth/repository/OAuth2AuthorizationRequestBasedOnCookieRepository.java
@@ -2,6 +2,7 @@ package com.cookietrip.domain.auth.repository;
 
 import com.cookietrip.global.util.CookieUtil;
 import com.nimbusds.oauth2.sdk.util.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
 import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
 
@@ -11,10 +12,17 @@ import javax.servlet.http.HttpServletResponse;
 public class OAuth2AuthorizationRequestBasedOnCookieRepository
         implements AuthorizationRequestRepository<OAuth2AuthorizationRequest> {
 
-    public final static String OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME = "oauth2_auth_request";
-    public final static String REDIRECT_URI_PARAM_COOKIE_NAME = "redirect_uri";
-    public final static String REFRESH_TOKEN_COOKIE_NAME = "refresh_token";
-    private final static int COOKIE_EXPIRE_SECONDS = 180;
+    @Value("${cookie.oauth2-authorization-request-cookie-name}")
+    private String OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME;
+
+    @Value("${cookie.redirect-uri-param-cookie-name}")
+    private String REDIRECT_URI_PARAM_COOKIE_NAME;
+
+    @Value("${cookie.refresh-token-cookie-name}")
+    private String REFRESH_TOKEN_COOKIE_NAME = "refresh_token";
+
+    @Value("${cookie.cookie-expire-seconds}")
+    private int COOKIE_EXPIRE_SECONDS;
 
     @Override
     public OAuth2AuthorizationRequest loadAuthorizationRequest(HttpServletRequest request) {
