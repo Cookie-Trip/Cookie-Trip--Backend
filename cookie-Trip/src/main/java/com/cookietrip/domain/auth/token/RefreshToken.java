7d5ac99777f521bc9391c5615563007b14b731b0
pepe6bf <pepe6bf@gmail.com>
2023-05-15 16:03:44 +0900

    refactor: #10 RefreshToken Entity 토큰 값 변경 메서드 추가
    - Refresh Token Entity에 기존 토큰값을 변경하는 메서드를 추가하였다.
    - MemberPersonalId -> memberPersonalId로 필드명을 변경하였다.
    - 생성자 & 펙토리 메서드를 추가해주었다.

--- cookie-Trip/src/main/java/com/cookietrip/domain/auth/token/RefreshToken.java
+++ cookie-Trip/src/main/java/com/cookietrip/domain/auth/token/RefreshToken.java
@@ -13,12 +13,35 @@ import javax.persistence.*;
 @Entity
 public class RefreshToken extends BaseTimeEntity {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(length = 100, nullable = false, unique = true, updatable = false)
-    private String MemberPersonalId;
+    private String memberPersonalId;
 
     @Column(length = 500, nullable = false)
     private String value;
+
+    private RefreshToken(
+            String memberPersonalId,
+            String value
+    ) {
+        this.memberPersonalId = memberPersonalId;
+        this.value = value;
+    }
+
+    public static RefreshToken of(
+            String memberPersonalId,
+            String value
+    ) {
+        return new RefreshToken(memberPersonalId, value);
+    }
+
+    /**
+     * Token Value 변경
+     */
+    public void changeTokenValue(String tokenValue) {
+        this.value = tokenValue;
+    }
 }
