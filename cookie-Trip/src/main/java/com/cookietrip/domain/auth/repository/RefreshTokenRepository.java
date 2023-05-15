bdebdca6471f8ca2c1feee720cb4704a1f8b3551
pepe6bf <pepe6bf@gmail.com>
2023-05-15 16:02:24 +0900

    feat: #10 RefreshTokenRepository 사용자 개인 ID값을 기반으로 리프레쉬 토큰을 조회하는 메서드 추가

--- cookie-Trip/src/main/java/com/cookietrip/domain/auth/repository/RefreshTokenRepository.java
+++ cookie-Trip/src/main/java/com/cookietrip/domain/auth/repository/RefreshTokenRepository.java
@@ -3,5 +3,10 @@ package com.cookietrip.domain.auth.repository;
 import com.cookietrip.domain.auth.token.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+import java.util.Optional;
+
+public interface RefreshTokenRepository
+        extends JpaRepository<RefreshToken, Long> {
+
+    Optional<RefreshToken> findByMemberPersonalId(String memberPersonalId);
 }
