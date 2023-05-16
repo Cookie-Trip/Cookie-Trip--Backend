948414f6fc43b251ce0f8cd5fe7a2c9d1fbf15de
pepe6bf <pepe6bf@gmail.com>
2023-05-16 14:24:08 +0900

    feat: #10 TokenAuthenticationFilter 구현
    토큰의 유효성을 검증하는 Servlet Filter인 TokenAuthenticationFilter Class를 개발하였다.

---
+++ cookie-Trip/src/main/java/com/cookietrip/domain/auth/filter/TokenAuthenticationFilter.java
@@ -0,0 +1,66 @@
+package com.cookietrip.domain.auth.filter;
+
+import com.cookietrip.domain.auth.exception.TokenException;
+import com.cookietrip.domain.auth.service.TokenService;
+import com.cookietrip.domain.auth.token.AuthToken;
+import com.cookietrip.global.util.HeaderUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static com.cookietrip.domain.auth.exception.AuthExceptionCode.REQUEST_TOKEN_NOT_FOUND;
+
+@RequiredArgsConstructor
+public class TokenAuthenticationFilter
+        extends OncePerRequestFilter {
+
+    private final TokenService tokenService;
+
+    private static final String EXCEPTION_ATTRIBUTE_NAME = "exceptionCode";
+    private static final String TOKEN_REISSUE_REQUEST_URI = "/api/auth/refresh";
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        // Access Token 값 파싱
+        String accessToken = HeaderUtil.getAccessToken(request);
+
+        // 요청에 토큰이 존재하는지 검증
+        if (accessToken == null) {
+            request.setAttribute(EXCEPTION_ATTRIBUTE_NAME, REQUEST_TOKEN_NOT_FOUND);
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        AuthToken authAccessToken = tokenService.createAuthTokenOfAccessToken(accessToken);
+
+        // Token 재발급 요청인 경우
+        String path = request.getRequestURI();
+        if (TOKEN_REISSUE_REQUEST_URI.equals(path)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        try {
+            if (authAccessToken.validate()) {
+                Authentication authentication = tokenService.getAuthentication(authAccessToken);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        } catch (TokenException e) {
+            request.setAttribute(EXCEPTION_ATTRIBUTE_NAME, e.getExceptionCode());
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
