e172777c7de194c8b452cb77bdeb017b21f8a5ab
pepe6bf <pepe6bf@gmail.com>
2023-05-16 17:20:49 +0900

    feat: #10 CustomAuthenticationEntryPoint 개발
    JWT 관련 예외가 발생할 시 예외를 잡는 CustomAuthenticationEntryPoint를 개발하였다.

---
+++ cookie-Trip/src/main/java/com/cookietrip/domain/auth/exception/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,50 @@
+package com.cookietrip.domain.auth.exception;
+
+import com.cookietrip.global.exception.ExceptionCode;
+import com.cookietrip.global.exception.ExceptionResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static com.cookietrip.domain.auth.exception.AuthExceptionCode.*;
+
+@Slf4j
+public class CustomAuthenticationEntryPoint
+        implements AuthenticationEntryPoint {
+
+    private static final String EXCEPTION_ATTRIBUTE_NAME = "exceptionCode";
+
+    @Override
+    public void commence(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AuthenticationException authException
+    ) throws IOException, ServletException {
+
+        ExceptionCode exceptionCode = (ExceptionCode) request.getAttribute(EXCEPTION_ATTRIBUTE_NAME);
+
+        // 사용자 정의 예외의 Exception Code(Enum)가 넘어온 경우
+        if (exceptionCode != null) {
+            setResponse(response, exceptionCode);
+        } else {    // Error Message 만 넘어온 경우 범용 인증 예외 코드를 설정
+            log.error("Responding with unauthorized error. Message := {}", authException.getMessage());
+            setResponse(response, AUTHENTICATION_ERROR);
+        }
+    }
+
+    // 에러 응답 생성 메소드
+    private void setResponse(
+            HttpServletResponse response,
+            ExceptionCode exceptionCode
+    ) throws IOException {
+        response.setContentType("application/json;charset=UTF-8");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        // 한글 출력을 위해 getWriter() 사용
+        response.getWriter().print(ExceptionResponse.of(exceptionCode).convertJson());
+    }
+}
