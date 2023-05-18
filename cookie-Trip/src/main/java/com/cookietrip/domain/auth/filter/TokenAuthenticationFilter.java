3b426afd3b1da5dba15f6e9e930f53b392b400ee
pepe6bf <pepe6bf@gmail.com>
2023-05-18 14:17:59 +0900

    style: #10 TokenAuthenticationFilter 불필요한 import 제거

--- cookie-Trip/src/main/java/com/cookietrip/domain/auth/filter/TokenAuthenticationFilter.java
+++ cookie-Trip/src/main/java/com/cookietrip/domain/auth/filter/TokenAuthenticationFilter.java
@@ -5,7 +5,6 @@ import com.cookietrip.domain.auth.service.TokenService;
 import com.cookietrip.domain.auth.token.AuthToken;
 import com.cookietrip.global.util.HeaderUtil;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
