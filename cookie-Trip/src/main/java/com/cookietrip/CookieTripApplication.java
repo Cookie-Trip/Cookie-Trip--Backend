cbf02bb81a6406dd80f6697a03de5090ccc05bb8
pepe6bf <pepe6bf@gmail.com>
2023-05-23 12:24:07 +0900

    refactor: #13 CookieTripApplication 불필요한 어노테이션 제거

--- cookie-Trip/src/main/java/com/cookietrip/CookieTripApplication.java
+++ cookie-Trip/src/main/java/com/cookietrip/CookieTripApplication.java
@@ -1,15 +1,12 @@
 package com.cookietrip;
 
-import com.cookietrip.domain.entity.Feed;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class CookieTripApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CookieTripApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CookieTripApplication.class, args);
+    }
 }
