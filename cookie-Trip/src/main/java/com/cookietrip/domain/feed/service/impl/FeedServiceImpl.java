f12276bdea3183d7e3a23d351cbd0dd9813f7f81
pepe6bf <pepe6bf@gmail.com>
2023-05-23 23:40:14 +0900

    feat: #13 위치 기반 추천 피드 조회 & 특정 피드 상세 조회 API 개발
    현재 사용자 위치를 기반으로 주변의 추천 피드를 조회하는 API와 특정 피드를 상세 조회하는 API를 개발하였다.

---
+++ cookie-Trip/src/main/java/com/cookietrip/domain/feed/service/impl/FeedServiceImpl.java
@@ -0,0 +1,61 @@
+package com.cookietrip.domain.feed.service.impl;
+
+import com.cookietrip.domain.feed.Repository.FeedImageRepository;
+import com.cookietrip.domain.feed.Repository.FeedRepository;
+import com.cookietrip.domain.feed.Repository.FeedReviewRepository;
+import com.cookietrip.domain.feed.dto.FeedDto;
+import com.cookietrip.domain.feed.dto.FeedImageDto;
+import com.cookietrip.domain.feed.dto.FeedReviewDto;
+import com.cookietrip.domain.feed.dto.FeedWithReviewsDto;
+import com.cookietrip.domain.feed.exception.FeedNotFoundException;
+import com.cookietrip.domain.feed.model.entity.Feed;
+import com.cookietrip.domain.feed.service.FeedService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class FeedServiceImpl
+        implements FeedService {
+
+    private final FeedRepository feedRepository;
+
+    private final FeedImageRepository feedImageRepository;
+
+    private final FeedReviewRepository feedReviewRepository;
+
+    @Override
+    public List<FeedDto> findFeedsByLocation(String searchLocation) {
+        return feedRepository.findAllByLocation_Title(searchLocation)
+                .stream()
+                .map(feed -> FeedDto.of(feed, findAllFeedImages(feed)))
+                .toList();
+    }
+
+    @Override
+    public FeedWithReviewsDto findFeed(Long feedId) {
+        return feedRepository.findById(feedId)
+                .map(feed -> FeedWithReviewsDto.of(
+                        feed,
+                        findAllFeedImages(feed),
+                        findAllFeedReviews(feed))
+                )
+                .orElseThrow(FeedNotFoundException::new);
+    }
+
+    private List<FeedImageDto> findAllFeedImages(Feed feed) {
+        return feedImageRepository.findAllByFeed(feed)
+                .stream()
+                .map(FeedImageDto::fromEntity)
+                .toList();
+    }
+
+    private List<FeedReviewDto> findAllFeedReviews(Feed feed) {
+        return feedReviewRepository.findAllByFeed(feed)
+                .stream()
+                .map(FeedReviewDto::fromEntity)
+                .toList();
+    }
+}
