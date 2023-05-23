e9ef2636576d94b4f713d3ddb582747b1db6f6ed
pepe6bf <pepe6bf@gmail.com>
2023-05-23 23:38:28 +0900

    feat: #13 FeedService 추상화
    Feed domain의 비즈니스 로직이 들어갈 FeedService Interface를 정의해주었다.

---
+++ cookie-Trip/src/main/java/com/cookietrip/domain/feed/service/FeedService.java
@@ -0,0 +1,19 @@
+package com.cookietrip.domain.feed.service;
+
+import com.cookietrip.domain.feed.dto.FeedDto;
+import com.cookietrip.domain.feed.dto.FeedWithReviewsDto;
+
+import java.util.List;
+
+public interface FeedService {
+
+    /**
+     * 특정 지역의 피드를 모두 DB 에서 조회
+     */
+    List<FeedDto> findFeedsByLocation(String searchLocation);
+
+    /**
+     * 특정 피드를 리뷰와 함께 조회
+     */
+    FeedWithReviewsDto findFeed(Long feedId);
+}
